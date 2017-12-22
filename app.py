8992ca0b3652250805e6054181fc072dfde2be26
David Awad <davidawad64@gmail.com>
2017-12-22 14:31:57 -0500

    DEV: AUTO COMMIT: Fri Dec 22 14:31:57 2017

--- app.py
+++ app.py
@@ -40,8 +40,7 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    send_message(sender_id, "roger that!")
-                    send_message(sender_id, "roger that second!")
+                    send_message(sender_id, "roger that! sending buttons...")
 
                     g = {}
                     g['buttons'] = [
