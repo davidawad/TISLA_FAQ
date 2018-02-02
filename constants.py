773c7416c2b87cf9225041967ff6e1d0af625525
David Awad <davidawad64@gmail.com>
2018-02-02 11:06:57 -0500

    DEV: AUTO COMMIT: Fri Feb  2 11:06:57 2018

--- constants.py
+++ constants.py
@@ -144,21 +144,21 @@ Our policy is to not recommend one lender over another, but coming soon is a lis
         },
       ],
 
-    }
+    },
 
- #  'buttons' : {
+ 'buttons' : {
 
-   #  "perkins_lf" : [
-     #  {
-       #  "type":  "web_url",
-       #  "url":   "http://freestudentloanadvice.org/loan-forgiveness/perkins-loan-forgiveness-program/",
-       #  "title": "Select Criteria",
-       #  "webview_height_ratio": "full",
-       #  "messenger_extensions": True,
-       #  "fallback_url": "http://freestudentloanadvice.org/loan-forgiveness/perkins-loan-forgiveness-program/"
-     #  }
+   "perkins_lf" : [
+     {
+       "type":  "web_url",
+       "url":   "http://freestudentloanadvice.org/loan-forgiveness/perkins-loan-forgiveness-program/",
+       "title": "Select Criteria",
+       "webview_height_ratio": "full",
+       "messenger_extensions": True,
+       "fallback_url": "http://freestudentloanadvice.org/loan-forgiveness/perkins-loan-forgiveness-program/"
+     }
 
-    #  ]
- #  }
+    ]
+ }
 
 }
