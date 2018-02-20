e4efe552928673c3f5ef037f751c7edf8aef30cf
David Awad <davidawad64@gmail.com>
2018-02-20 10:45:04 -0500

    BOT: AUTO COMMIT: Tue Feb 20 10:45:04 2018

--- constants.py
+++ constants.py
@@ -41,6 +41,28 @@ input_message_key_mappings = {
 
          # consolidate my loans?
          "Should I consolidate my loans?": 'consolidate_1',
+
+         # fraud
+         'fraud' : 'fraud',
+         'borrower defense' : 'fraud',
+         'scams' : 'fraud',
+
+
+         'unpaid refund' : 'unpaid',
+
+         # identity theft
+         "id theft": 'id',
+         "identity": 'id',
+         "identity theft": 'id',
+         'false certification': 'id',
+
+         # school closed
+         'school closed' : 'school_closed',
+
+         # D & D
+         'death' : 'death',
+         'disability' : 'disability',
+
          "Thanks" : 'thank_you'
         }
 
@@ -51,7 +73,7 @@ raw_response_data = {
 
   'responses' : {
 
-      'intro' : "Hello! Thanks for reaching out to TISLA. You can walk through our guide to get advice on your student loans. Send 'restart' at any time to restart. Or just visit us at http://freestudentloanadvice.org",
+      'intro' : "Hello!  I’m Sloan, your automated guide for advice on your student loans brought to you by TISLA.  This is not legal advice but simple guidance to help you manage your student loan debt.  Send 'restart' at any time to restart.  Ready?  Simply type your question and I’ll try and guide you to the best way to manage your student debt!",
 
 
     'loan_forgiveness' : "First the bad news, most borrowers should expect to repay their student loans as agreed. With that said, there are many student loan repayment, discharge and forgiveness programs out there. Over time, we plan on having a comprehensive list, but for now, we'll just talk about the most common programs.",
@@ -59,7 +81,7 @@ raw_response_data = {
 
     'pslf' : """ Public Service Loan Forgiveness (PSLF) tends to be the most commonly known federal student loan forgiveness program. Some also call it the “Obama Forgiveness Program” although you should note that nobody with any real knowledge of the student loan programs will ever call it that. In fact, this misnomer is one of the clues we use to identify potential student loan scams. You should too.
 
-To be eligible to get the balance of your loan forgiven under PSLF, the borrower will need to make 120 payments, on eligible loans, while working full time for an eligible employer. It’s important to note that all three of these things have to happen at the same time. Once you complete the 120 payments, the balance of your loans are forgiven, tax free.""",
+    To be eligible to get the balance of your loan forgiven under PSLF, the borrower will need to make 120 payments, on eligible loans, while working full time for an eligible employer. It’s important to note that all three of these things have to happen at the same time. Once you complete the 120 payments, the balance of your loans are forgiven, tax free. You can find more information on PSLF on our website here:  http://freestudentloanadvice.org/loan-forgiveness/""",
 
 
     'teacher_lf' : """Teacher Loan Forgiveness (TLF) pays up to $17,500 in eligible federal student loans for qualified teachers who teach in a Title I school for five consecutive years.
@@ -118,9 +140,23 @@ If you find yourself unable to pay, one strategy is to pay as much as you can, e
 
 
 
+   'fraud' : 'Sometimes bad things happen to good people. Thankfully, there are often remedies for these things for borrowers of federal student loans. For the most part, student loan discharges are for situations where the loan should not have been made in the first place or for when something terrible happens, such as the death or disability of the borrower. Below are the requirements for the discharges that are currently available. You should note that in several of these cases, the discharged amount is taxed as income. Take a look at our page on loan forgiveness for more information: http://freestudentloanadvice.org/loan-forgiveness/',
+
+
+   'id' : 'You can find more information on what to do about false identification on our site here: http://freestudentloanadvice.org/loan-forgiveness/false-certification/',
+
+
+   'school_closed' : 'If your school has closed there may be other options, take a look on our site: http://freestudentloanadvice.org/loan-forgiveness/closed-school-discharge/',
+
+
+   'death' : 'If the borrower of the loan, or, in the case of a Parent Plus loan either the borrower or the student for whom the loan was taken should pass away, the loan will be fully discharged. To apply, first contact the loan holder by phone, who will put the loan on hold for up to sixty days. Submit to them a certified copy of the death certificate to obtain the discharge. If there are loans at multiple loan holders, you will need to notify and submit these certificates to all of them. Note that the amount of the discharge may be taxed to the deceased estate as income: http://freestudentloanadvice.org/loan-forgiveness/death-discharge',
+
+
+   'disability' : "Total and Permanent Disability Discharge (TPD) can be difficult to obtain so it’s very important to follow all instructions and respond to all requests in a timely way. This is due to significant fraud found in past audits of the program. Only the actual borrower of the loans disability status can be submitted for discharge review. All borrowers applying for this discharge must do so at http://www.disabilitydischarge.com. There's more information available here: http://freestudentloanadvice.org/loan-forgiveness/total-and-permanent-disability-discharge/",
+
+   'unpaid': "If you withdrew from school before completing 60% of the academic year or loan period, the school may have been required to return all or a portion of your loan to the loan holder. There's more information here: http://freestudentloanadvice.org/loan-forgiveness/unpaid-refund-discharge/",
+
    'thank_you': "Thank you! Please feel free to reach out to us anytime either through messenger or on the web.",
-
-
   },
 
 
@@ -158,7 +194,6 @@ If you find yourself unable to pay, one strategy is to pay as much as you can, e
            "content_type": "text",
            "title": "Public Service Loan Forgiveness",
            "payload": "<POSTBACK_PAYLOAD>",
-           # "image_url": ""
          },
          {
            "content_type": "text",
@@ -276,6 +311,46 @@ If you find yourself unable to pay, one strategy is to pay as much as you can, e
         {
           "content_type": "text",
           "title": "I'm not sure",
+          "payload": "<POSTBACK_PAYLOAD>"
+        },
+      ],
+
+
+    # should I consolidate
+    'fraud' : [
+        {
+          "content_type": "text",
+          "title": "Closed School",
+          "payload": "<POSTBACK_PAYLOAD>"
+        },
+        {
+          "content_type": "text",
+          "title": "Borrower Defense",
+          "payload": "<POSTBACK_PAYLOAD>"
+        },
+        {
+          "content_type": "text",
+          "title": "False Certification", # TODO
+          "payload": "<POSTBACK_PAYLOAD>"
+        },
+        {
+          "content_type": "text",
+          "title": "Death",
+          "payload": "<POSTBACK_PAYLOAD>"
+        },
+        {
+          "content_type": "text",
+          "title": "Disability",
+          "payload": "<POSTBACK_PAYLOAD>"
+        },
+        {
+          "content_type": "text",
+          "title": "Unpaid Refund", #TODO
+          "payload": "<POSTBACK_PAYLOAD>"
+        },
+        {
+          "content_type": "text",
+          "title": "Bankruptcy",
           "payload": "<POSTBACK_PAYLOAD>"
         },
       ],
